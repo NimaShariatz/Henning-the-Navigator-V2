266e95ec0aa96e6028040b30deb5feccb4c6657c
NimaShariatz <shariatz@ualberta.ca>
2026-08-03 16:21:05 -0600

    login woork

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 - Create a virtual environment
 - CD to /backend and `pip install -r requirements.txt` followed by `python manage.py runserver`
 - CD to /frontend and `npm install` followed by `npm run dev`
+
+
+# ToDO
+- tests run by gh-pages
+- a dev and prod branch with live instances
+- gunicorn
+- NGINX
+- AWS instance
+- AWS postgres
+- Docker
--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -2,6 +2,7 @@ import styles from './Login.module.css';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
 import { loginUser } from '../../api/Login';
+import { HennLogo2 } from '../../constants';
 
 function Login() {
   const [username, setUsername] = useState('');
@@ -24,31 +25,49 @@ function Login() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <input
-          required
-          type="text"
-          name="username"
-          placeholder="Username"
-          autoComplete="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          required
-          type="password"
-          name="password"
-          minLength={8}
-          placeholder="Password"
-          autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className={styles.loginButton} type="submit">
-          Submit
-        </button>
-      </form>
+      <div className={styles.coreContainer}>
+        <div className={styles.container}>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <div>
+              <label>Username</label>
+              <input
+                required
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Password</label>
+              <input
+                required
+                type="password"
+                name="password"
+                minLength={8}
+                placeholder="Password"
+                autoComplete="new-password"
+                pattern="^(?!^\d+$).{8,}$"
+                title="Must be at least 8 characters and not entirely numeric"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <button className={styles.signupButton} type="submit">
+              Login
+            </button>
+          </form>
+        </div>
+
+        <div
+          className={styles.home_background_img}
+          style={{ backgroundImage: `url(${HennLogo2})` }}
+        ></div>
+      </div>
     </>
   );
 }
